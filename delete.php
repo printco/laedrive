9e64ca24fbda59d89ac4a3cd9e1b1e0240ec668b
nattapon khunpakdee <nattapon@sycapt.com>
2022-11-01 23:27:59 +0700

    can delete

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+
+$entry = $_GET['entry'];
+$target = "./upload/";
+if( is_file("$target$entry") ) {
+	if( unlink("$target$entry") ){
+		echo "Delete $entry success";
+	}
+}
+
--- index.php
+++ index.php
@@ -1,15 +1,60 @@
 <?php
+$upload = false;
+$css = "";
 if( isset($_FILES['uplfile']) ){
-	if( is_uploaded_file($_FILES['uplfile']['tmp_name']) ){
-		if( move_uploaded_file($_FILES['uplfile']['tmp_name'],"./upload/".$_FILES['uplfile']['name']) ){
-			echo "<div>Upload success</div>";
+	$upload = true;
+	$f = $_FILES['uplfile'];
+	$msg = "";
+	$css = "error";
+	if( $f['error'] == 1 ){
+		$msg = "Error " . $f['error'];
+	} else {
+		if( is_uploaded_file($f['tmp_name']) ){
+			if( move_uploaded_file($f['tmp_name'],"./upload/".$f['name']) ){
+				
+				$css = "success";
+				$msg = "Upload file success";
+			}
 		}
 	}
 }
 ?>
 <html>
 	<title>Lae Drive</title>
+	<head>
+	<style>
+	body{
+		font-family: sans-serif;
+		font-size: 13px;
+	}
+	.success {
+		background: #d6f3d6;
+		padding: 5px;
+		width: 300px;
+		text-align: center;
+		margin: 5px;
+		border: 1px solid green;
+		font-weight: bold;
+		color: green;
+	}
+	.error {
+		background: #efe3d8;
+		padding: 5px;
+		width: 300px;
+		text-align: center;
+		margin: 5px;
+		border: 1px solid #c77955;
+		font-weight: bold;
+		color: #ef7242;
+	}
+	</style>
+	</head>
 	<body>
+		<?php
+		if( $upload ){
+			echo "<div class='msg $css'>$msg</div>";
+		}
+		?>
 		<form method="post" enctype="multipart/form-data">
 			<input type="file" name="uplfile" />
 			<input type="submit" value="Upload" />
@@ -23,7 +68,11 @@ if( isset($_FILES['uplfile']) ){
 				if( $entry == '..' || $entry == '..' ) continue;
 				$full_path = "./upload/$entry";
 				if( is_file($full_path) ){
-					echo "<li><a href='$full_path'>$entry</a></li>";
+					$del = "<a href='delete.php?entry=$entry' style='color:red'>Delete</a>";
+					if( $entry == "readme.txt" ){
+						$del = "";
+					}
+					echo "<li><a href='$full_path'>$entry</a>&nbsp;$del</li>";
 				}
 			}
 			closedir($dir);
