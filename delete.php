61eea190f32632c4e81e297501a643e9ccd2f19f
nattapon khunpakdee <nattapon@sycapt.com>
2022-11-01 23:40:39 +0700

    add div

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ $entry = $_GET['entry'];
 $target = "./upload/";
 if( is_file("$target$entry") ) {
 	if( unlink("$target$entry") ){
-		echo "Delete $entry success";
+		echo "<div>Delete $entry success</div>";
 	}
 }
-echo "<a href='index.php'>Home</a>";
+echo "<div><a href='index.php'>Home</a></div>";
