a8f8958b93417492dfd5c3c31176ce64ca681286
nattapon khunpakdee <nattapon@sycapt.com>
2023-02-08 14:23:41 +0700

    add team meeting

--- index.php
+++ index.php
@@ -45,6 +45,9 @@ $post_max_size = ini_get("post_max_size");
 				<div class="col-2"></div>
 			</div>
 		</div>
+		<div id="msteam-meet">
+			<a href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTljYzY3ZDEtMjMyOC00MmQ5LWIwMDAtZTExZDBiNWMwYmFm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522771863e0-ae69-42c9-9bb8-40e48d597651%2522%252c%2522Oid%2522%253a%2522cd50b393-2000-4c59-916d-f069a091654d%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=7651a5dd-34fc-4f29-abe7-c49fa0f95f3d&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" target="_blank">Meeting</a>
+		</div>
 		<div>
 			<ul id="filelist">
 			<?php
