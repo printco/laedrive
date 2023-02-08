20c76fdcc0da6a61de370fbb4e05751297f589e6
nattapon khunpakdee <nattapon@sycapt.com>
2023-02-08 18:37:27 +0700

    size & hash

--- list.php
+++ list.php
@@ -7,6 +7,7 @@ while( $entry = readdir() ){
 	if( is_file($full_path) ){
 		$file_date = filemtime($full_path);
 		$file_size = filesize($full_path);
+		$hash_file = hash_file("SHA1",$full_path);
 		$file_kbsize = number_format(round($file_size / 1024,2),2);
 		$file_ymd = date("d/m/Y H:i:s",$file_date);
 		$del = "<a href='delete.php?entry=$entry' style='color:red' onclick='return confirmDelete();'>Delete</a>";
@@ -15,7 +16,7 @@ while( $entry = readdir() ){
 			$del = "";
 			$jsConfirm = "";
 		}
-		echo "<tr><td><a href='$full_path' $jsConfirm>$entry</a></td><td>$file_ymd</td><td style='text-align:right'>$file_kbsize</td><td>$del</td></tr>";
+		echo "<tr><td><a href='$full_path' $jsConfirm>$entry</a></td><td>$file_ymd</td><td style='text-align:right'>$file_kbsize</td><td>$hash_file</td><td>$del</td></tr>";
 	}
 }
 closedir($dir);
