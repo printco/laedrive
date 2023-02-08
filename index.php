a5189536971bb3cf6d338e0349029267ff1ed4d6
nattapon khunpakdee <nattapon@sycapt.com>
2023-02-08 15:06:18 +0700

    meet button

--- index.php
+++ index.php
@@ -12,6 +12,21 @@ $post_max_size = ini_get("post_max_size");
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="upload.js"></script>
 	<script type="text/javascript" src="jquery.js"></script>
+	<script>
+	function confirmMeet(){
+		if( confirm("Meeet?") ){
+			return true;
+		}
+		return false;
+	}
+	
+	function confirmDownload(){
+		if( confirm("Download?") ){
+			return true;
+		}
+		return false;
+	}
+	</script>
 	</head>
 	<body>
 		<div class='noti' style="padding:5px 0 5px 5px;">
@@ -46,14 +61,15 @@ $post_max_size = ini_get("post_max_size");
 			</div>
 		</div>
 		<div id="msteam-meet">
-			<a href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTljYzY3ZDEtMjMyOC00MmQ5LWIwMDAtZTExZDBiNWMwYmFm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522771863e0-ae69-42c9-9bb8-40e48d597651%2522%252c%2522Oid%2522%253a%2522cd50b393-2000-4c59-916d-f069a091654d%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=7651a5dd-34fc-4f29-abe7-c49fa0f95f3d&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" target="_blank">Meeting</a>
+			<a href="https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_ZTljYzY3ZDEtMjMyOC00MmQ5LWIwMDAtZTExZDBiNWMwYmFm%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522771863e0-ae69-42c9-9bb8-40e48d597651%2522%252c%2522Oid%2522%253a%2522cd50b393-2000-4c59-916d-f069a091654d%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=7651a5dd-34fc-4f29-abe7-c49fa0f95f3d&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true" target="_blank" onclick="return confirmMeet()">Meeting</a>
 		</div>
-		<div>
-			<ul id="filelist">
+		<div id="filelist">
+			<table>
+				<tr><th>File Name</th><th>File Date</th><th>&nbsp;</th></tr>
 			<?php
 				require_once "list.php";
 			?>
-			</ul>
+			</table>
 		</div>
 	</body>
 </html>
--- list.php
+++ list.php
@@ -5,11 +5,15 @@ while( $entry = readdir() ){
 	if( $entry == '..' || $entry == '..' ) continue;
 	$full_path = "./upload/$entry";
 	if( is_file($full_path) ){
+		$file_date = filemtime($full_path);
+		$file_ymd = date("d/m/Y H:i:s",$file_date);
 		$del = "<a href='delete.php?entry=$entry' style='color:red' onclick='return confirmDelete();'>Delete</a>";
+		$jsConfirm = "onclick=\"return confirmDownload()\"";
 		if( $entry == "readme.txt" ){
 			$del = "";
+			$jsConfirm = "";
 		}
-		echo "<li><a href='$full_path'>$entry</a>&nbsp;$del</li>";
+		echo "<tr><td><a href='$full_path' $jsConfirm>$entry</a></td><td>$file_ymd</td><td>$del</td></tr>";
 	}
 }
 closedir($dir);
